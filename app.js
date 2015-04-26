77d473e57880ff68991e651e9688db023cd29db9
mguida22 <michael.guida@colorado.edu>
2015-04-25 21:44:16 -0600

    Adding support to get current company name and title from LinkedIn

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function getInfo() {
 		alert("Please log in first");
 	} else {
 		IN.API.Profile("me")
-			.fields(["firstName","lastName", "location:(name)", "skills:(skill:(name))", "picture-url"])
+			.fields(["firstName","lastName", "location:(name)", "skills:(skill:(name))", "picture-url", "three-current-positions:(title,company:(name))"])
 		    .result(function(result) {
 		    	console.log(JSON.stringify(result));
 		    	displayInfo(result);
@@ -28,8 +28,7 @@ function getInfo() {
 function displayInfo(data) {
 	console.log("Displaying info..");
 	document.getElementById("name").innerHTML = data.values[0].firstName + " " + data.values[0].lastName;
-	document.getElementById("title").innerHTML = "";
-	document.getElementById("company").innerHTML = "";
+	document.getElementById("position").innerHTML = data.values[0].threeCurrentPositions.values[0].title + " at " + data.values[0].threeCurrentPositions.values[0].company.name;
 	//Display up to three skills
 	for (var i=0; i<3; i++){
 		if (data.values[0].skills.values[i].skill.name != undefined){
