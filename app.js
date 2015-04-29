cb855bd213f821b2773a0cf0688714bf6236659c
mguida22 <michael.guida@colorado.edu>
2015-04-29 01:34:45 -0600

    Allowing for inline editing

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ function getInfo() {
 		alert("Please log in first");
 	} else {
 		IN.API.Profile("me")
-			.fields(["firstName","lastName", "location:(name)", "skills:(skill:(name))", "picture-url", "three-current-positions:(title,company:(name))"])
+			.fields(["firstName","lastName", "location:(name)", "skills:(skill:(name))",
+				"picture-url", "positions:(title,company:(name))",
+				"public-profile-url", "email-address"])
 		    .result(function(result) {
 		    	console.log(JSON.stringify(result));
 		    	displayInfo(result);
@@ -27,14 +29,19 @@ function getInfo() {
 
 function displayInfo(data) {
 	console.log("Displaying info..");
-	document.getElementById("name").innerHTML = data.values[0].firstName + " " + data.values[0].lastName;
-	document.getElementById("position").innerHTML = data.values[0].threeCurrentPositions.values[0].title + " at " + data.values[0].threeCurrentPositions.values[0].company.name;
-	//Display up to three skills
+	//document.getElementById("name").innerHTML = data.values[0].firstName + " " + data.values[0].lastName;
+	document.getElementById("name").value = data.values[0].firstName + " " + data.values[0].lastName;
+	document.getElementById("position").value = data.values[0].positions.values[0].title + " at " + data.values[0].positions.values[0].company.name;
+	
+	//Clear old skills and display up to three skills
+	document.getElementById("skills").value = "";
 	for (var i=0; i<3; i++){
 		if (data.values[0].skills.values[i].skill.name != undefined){
-			document.getElementById("skills").innerHTML += data.values[0].skills.values[i].skill.name + " ";
+			document.getElementById("skills").value += data.values[0].skills.values[i].skill.name + " ";
 		}
 	}
-	document.getElementById("location").innerHTML = data.values[0].location.name;
-	document.getElementById("image").innerHTML = "<img src=\"" + data.values[0].pictureUrl+ "\" alt=\"user's image\">";
+	document.getElementById("profile").value = data.values[0].publicProfileUrl;
+	document.getElementById("email").value = data.values[0].emailAddress;
+	document.getElementById("location").value = data.values[0].location.name;
+	document.getElementById("image").innerHTML = "<img src=\"" + data.values[0].pictureUrl+ "\" alt=\"user's image\" style=\"float:right\">";
 }
