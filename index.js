b6a41bbdd2b702db1c0862c1ab7433660f74f2f8
mguida22 <michael.guida@colorado.edu>
2015-05-03 12:52:39 -0600

    Minor refactoring

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.get('/', function(request, response) {
   response.send(__dirname + '/public/index.html');
 });
 
-app.use(express.static(__dirname, 'public'));
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
